5147c012041b2fb08797d540519da489ca8118ca
Shay Neufeld <shayneufeld@gmail.com>
2017-07-28 15:03:16 -0700

    added scrapers and notebooks

---
+++ scrapers/bandcamp_spider/bandcamp_spider/items.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class BandcampAlbum(scrapy.Item):
+    # class for each album
+    
+    # define the fields for your item here like:
+    #album_name = scrapy.Field()
+    #album_url = scrapy.Field()
+    #contributor_name = scrapy.Field()
+    #contributor_url = scrapy.Field()
+    
+    
+    pass
