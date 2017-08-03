98403ea847dc0b9c6255e6a39e78b36bd640d44a
Shay Neufeld <shayneufeld@gmail.com>
2017-08-03 09:42:56 -0700

    working on new algorithm pipeline

--- flaskapp/pricecamp_app/a_Model.py
+++ flaskapp/pricecamp_app/a_Model.py
@@ -12,6 +12,8 @@ import numpy as np
 all_genres = pd.read_csv('/Users/shayneufeld/Dropbox/insight/pricecamp/all_genres.csv',index_col=0)
 data = pd.read_csv('/Users/shayneufeld/Dropbox/insight/pricecamp/master_table_trimmed.csv',index_col=0)
 features = ['sp_popularity','sp_num_followers']
+supporters_sparse = scipy.sparse.load_npz('/Users/shayneufeld/Dropbox/insight/pricecamp/data/supporters_sparse_matrix.npz')
+artist_table = pd.read_csv('/Users/shayneufeld/Dropbox/insight/pricecamp/data/artist_table.csv')
 
 def predictNumBuyers(fromUser  = 'Default', rel_artists = [],artist_name=[],data=data,features=features):
     x = rel_artists.loc[:,features].values
@@ -36,23 +38,22 @@ def predictBestPrice(fromUser  = 'Default',y_predict=0):
     return price
 
 
-def getRelatedArtists(artist_name,data=data,all_genres=all_genres):
-    df = data.copy()
-    artist_genres_str = df[df.bc_artist == artist_name].sp_genres.values[0]
+def getRelatedArtists(artist_name,data=supporters_sparse):
+
+    artist_factors, _, user_factors = linalg.svds(bm25_weight(data), 50)
     
-    artist_genres = artist_genres_str.replace('[','')
-    artist_genres = artist_genres.replace(']','')
-    artist_genres = artist_genres.split(sep=',')
+    toprelated = TopRelated(artist_factors)
+    result = toprelated.get_related(678,N=21)
+    related_artists = []
+    related_tags = []
+    for r in result:
+        rel_artist = artist_table[artist_table.artist_id==r]
+        artist_name = rel_artist.artist_name.values
+        tags = rel_artist.tags.values
+        related_artists.append(artist_name)
+        related_tags.append(tags)
     
-    hits = []
-    for genre in artist_genres:
-        for i,genre_str in enumerate(data.sp_genres.values):
-            if genre in genre_str:
-                hits.append(i)
-    
-    results = df.iloc[hits].drop_duplicates()
-    
-    results = results[results.bc_artist != artist_name] #remove self from list
+    results = related_artists[1:] #remove self from list
     
     return results
 
@@ -61,3 +62,39 @@ def get_features(artist_name,data,features):
     y_test = data.loc[data.bc_artist == artist_name,'bc_avg_supporters'].values
     
     return x_test,y_test
+
+"""
+Functions for retrieving related artists
+"""
+from scipy.sparse import coo_matrix
+import scipy
+from scipy.sparse import linalg
+
+def bm25_weight(X, K1=100, B=0.8):
+    """ Weighs each row of a sparse matrix X  by BM25 weighting """
+    # calculate idf per term (user)
+    X = coo_matrix(X)
+
+    N = float(X.shape[0])
+    idf = np.log(N / (1 + np.bincount(X.col)))
+
+    # calculate length_norm per document (artist)
+    row_sums = np.ravel(X.sum(axis=1))
+    average_length = row_sums.mean()
+    length_norm = (1.0 - B) + B * row_sums / average_length
+
+    # weight matrix rows by bm25
+    X.data = X.data * (K1 + 1.0) / (K1 * length_norm[X.row] + X.data) * idf[X.col]
+    return X
+
+
+class TopRelated(object):
+    def __init__(self, artist_factors):
+        # fully normalize artist_factors, so can compare with only the dot product
+        norms = np.linalg.norm(artist_factors, axis=-1)
+        self.factors = artist_factors / norms[:, np.newaxis]
+
+    def get_related(self, artistid, N=20):
+        scores = self.factors.dot(self.factors[artistid])
+        best = np.argpartition(scores, -N)[-N:]
+        return sorted(zip(best, scores[best]), key=lambda x: -x[1])
